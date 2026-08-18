a780c1c028dcc9d20bd9a06207b1411b7af8d4c8
Prathul V <iamprathul@gmail.com>
2026-08-18 10:50:42 +0530

    image isssue rectified

--- backend/utils/gridfs.js
+++ backend/utils/gridfs.js
@@ -1,62 +1,66 @@
 const mongoose = require('mongoose')
 const { GridFSBucket, ObjectId } = require('mongodb')
 
-let bucket
+let bucket = null
 
-exports.getBucket = () => {
+const getBucket = () => {
 
     if (!mongoose.connection.db) {
         throw new Error('MongoDB connection is not ready')
     }
 
     if (!bucket) {
-        bucket = new GridFSBucket(mongoose.connection.db, {
-            bucketName: 'images'
-        })
+        bucket = new GridFSBucket(
+            mongoose.connection.db,
+            {
+                bucketName: 'images'
+            }
+        )
     }
 
     return bucket
 }
 
-exports.uploadImage = (file) => {
+const uploadImage = (file) => {
 
     return new Promise((resolve, reject) => {
-
-        const bucket = getBucket()
-
-        const uploadStream = bucket.openUploadStream(
-            file.originalname,
-            {
-                contentType: file.mimetype
-            }
-        )
-
-        uploadStream.on('error', reject)
-
-        uploadStream.on('finish', () => {
-            resolve(uploadStream.id.toString())
-        })
-
-        uploadStream.end(file.buffer)
-    })
-}
-
-exports.deleteImage = (fileId) => {
-
-    return new Promise(async (resolve, reject) => {
 
         try {
 
             const bucket = getBucket()
 
-            await bucket.delete(
-                new ObjectId(fileId)
+            const uploadStream = bucket.openUploadStream(
+                file.originalname,
+                {
+                    contentType: file.mimetype
+                }
             )
 
-            resolve()
+            uploadStream.on('error', reject)
+
+            uploadStream.on('finish', () => {
+                resolve(uploadStream.id.toString())
+            })
+
+            uploadStream.end(file.buffer)
 
         } catch (error) {
             reject(error)
         }
     })
+}
+
+const deleteImage = async (fileId) => {
+
+    const bucket = getBucket()
+
+    await bucket.delete(
+        new ObjectId(fileId)
+    )
+}
+
+module.exports = {
+    getBucket,
+    uploadImage,
+    deleteImage
 }
